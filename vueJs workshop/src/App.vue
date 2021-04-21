86368429dd0f3adab51e94702eb26da2a0467bca
Zouhairrrr <77061268+Zouhairrrr@users.noreply.github.com>
2021-04-21 15:22:03 +0000

    commit

---
+++ vueJs workshop/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid">
+    <navBar />
+    <sign />
+  </div>
+</template>
+
+<script>
+import navBar from "./components/navBar";
+import sign from "./views/sign";
+export default {
+  name: "App",
+  components: {
+    navBar,
+    sign,
+  },
+};
+</script>
---
+++ vueJs workshop/src/components/navBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="about">
+    <nav class="ab navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand abc" href="#">YouCode YOUSSOUFIA</a>
+      <div class="a collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="b form-inline my-2 my-lg-0">
+          <!-- <router-link class="btn btn-primary" to="/">SingUp</router-link> |
+        <router-link class="btn btn-primary" to="/login">SingIn</router-link> -->
+          <router-link
+            to="/"
+            class="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+            >Sign Up</router-link
+          >
+          <router-link
+            to="/login"
+            class="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+            >Sign In</router-link
+          >
+        </form>
+      </div>
+    </nav>
+  </div>
+</template>
+<style scoped>
+.ab {
+  margin-top: 20px;
+}
+.a {
+  display: flex !important;
+  justify-content: flex-end !important;
+}
+.b {
+  width: 30%;
+  display: flex !important;
+  justify-content: space-evenly !important;
+}
+.btn {
+  color: rgb(0, 0, 0) !important;
+}
+.btn:hover {
+  background-color: rgb(0, 0, 0) !important;
+  color: blue !important;
+}
+.abc {
+  color: blue !important;
+}
+</style>
