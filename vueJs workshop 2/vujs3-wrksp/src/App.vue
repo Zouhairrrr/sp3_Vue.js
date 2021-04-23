06c537f0dcdb6dfa00a5315afdf5cdbbd24f29c6
Zouhairrrr <77061268+Zouhairrrr@users.noreply.github.com>
2021-04-23 15:57:47 +0000

    commit

---
+++ vueJs workshop 2/vujs3-wrksp/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <Home/>
+      <About/>
+      <Experience/>
+      <Skills/>
+      <MyProject/>
+      <Footer/> 
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+import MyProject from "./components/MyProject";
+import Footer from "./components/Footer";
+
+
+
+export default {
+  name: "App",
+  components: {
+    Home,
+    About,
+    Experience,
+    Skills,
+    MyProject,
+    Footer
+  }
+};
+</script>
---
+++ vueJs workshop 2/vujs3-wrksp/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
+
+//   {
+//     path: "/about",
+//     name: "About",
+    
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+//   },
