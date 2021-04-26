8e0c479c37b1aa48347c5b61c6b1040e11d5a77a
YassineAzedine <77061498+YassineAzedine@users.noreply.github.com>
2021-04-26 15:25:59 +0000

    Update About.vue

--- vueJs workshop 2/vujs3-wrksp/src/components/About.vue
+++ vueJs workshop 2/vujs3-wrksp/src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   
-       <div class="container">
+       <div class="container" style="padding:50px 0">
   <div class="row">
     <div class="col"><div class="container">
   <div class="row row-cols-2">
@@ -22,8 +22,8 @@
   </div>
   <div class="row " style="">
     <div class="col-lg-8 col-md-6 col-xd-12 one text-center "  >
-        <h4 class="text-center">WHO AMI?</h4>
-     <p class="text-capitalize"><span> Lorem ipsum dolor,
+        <h4 class="">WHO AMI?</h4>
+     <p class="text-uppercase  text-justify"><span>  <strong>Lorem ipsum dolor,
             sit amet consectetur adipisicing elit. 
              Ex eaque eveniet repudiandae,   
            Culpa unde cumque dicta obcaecati fuga   
@@ -34,8 +34,8 @@
       
               
               
-             tenetur magnam quas voluptatibus quam . </span>
-        <p class="text-capitalize">  Ex eaque eveniet repudiandae, commodi rerum 
+             tenetur magnam quas voluptatibus quam .</strong>  </span>
+        <p class="text-uppercase text-justify">  Ex eaque eveniet repudiandae, commodi rerum 
            Culpa unde cumque dicta obcaecati fuga exer 
             itaque eveniet dolore, necessitatibus pers  
             sit amet consectetur adipisicing elit. 
@@ -59,26 +59,26 @@
              </p>  </div>
             
     <div class="col-lg-4 col-md-6 col-xd-12 text-capitalize "  >
-            <h4 class="text-center">HUGO BOLLON</h4> <br>
+            <h4 class="text-center"> <strong>HUGO BOLLON</strong> </h4> <br>
              
           
-                 <div class="medi">
-                     <p class=""> <strong>Email:</strong> hgmaugo.bollon@gmail.com </p>  
-              <p class=""> <strong>Phone:</strong> +121 6737363839939</p>  
-               <p class=""> <strong>City:</strong>Youssoufia,Maroc</p>  
-                <p class=""> <strong>Languages:</strong> Francais,Anglais </p>  
+                 <div class="medi ">
+                  <div> <p class=""> <strong>Email:</strong> hgmaugo.bollon@gmail.com </p> </div>   
+        <div> <p class=""> <strong>Phone:</strong> +121 6737363839939</p>  </div>
+               <div> <p class=""> <strong>City:</strong>Youssoufia,Maroc</p>  </div>
+             <div> <p class=""> <strong>Languages:</strong> Francais,Anglais </p>  </div>
              
            
          
                  </div>
                  
-              <ul class="text-center">
-                  <li>
-                      <a href=""> 
-                                  <img src="https://img.icons8.com/material-sharp/24/000000/facebook-circled--v3.png"/>
-                                  <img src="https://img.icons8.com/windows/32/000000/instagram-new.png"/>
-                                  <img src="https://img.icons8.com/ios-filled/26/000000/linkedin.png"/>
-                                  <img src="https://img.icons8.com/ios-glyphs/30/000000/github.png"/>
+                <ul class="text-center">
+                  <li >
+                      <a href="" > 
+                                  <img src="https://img.icons8.com/material-sharp/24/000000/facebook-circled--v3.png" style="width:50px"/>
+                                  <img src="https://img.icons8.com/windows/32/000000/instagram-new.png" style="width:50px"/>
+                                  <img src="https://img.icons8.com/ios-filled/26/000000/linkedin.png" style="width:50px"/>
+                                  <img src="https://img.icons8.com/ios-glyphs/30/000000/github.png" style="width:50px"/>
                        </a>
                    
                    </li>        
