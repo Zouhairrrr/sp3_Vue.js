4f9c2a67847156e11de558cb734311b98953fb32
YassineAzedine <77061498+YassineAzedine@users.noreply.github.com>
2021-04-26 16:33:09 +0000

    Update MyProject.vue

--- vueJs workshop 2/vujs3-wrksp/src/comp/MyProject.vue
+++ vueJs workshop 2/vujs3-wrksp/src/comp/MyProject.vue
@@ -1,5 +1,206 @@
 <template>
-   <h1>Hello world</h1>
+   <div class="container-fluid" style="background-color: #2c3e50">
+    <!-- Content here -->
+    <div class="container text-white">
+      <h1 class="text-center"><strong>My-Project</strong></h1>
+      <h5 class="text-center">This My Story</h5>
+      <hr />
+      <br />
+
+      <div class="container" style="width: 50%">
+        <div class="row">
+          <div class="col-sm">
+            <div class="d-grid gap-2 d-md-block">
+              <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div class="col-sm">
+            <button class="btn btn-primary" type="button">Library</button>
+          </div>
+          <div class="col-sm">
+            <h3>GO-DELIB</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 container" style="width: 80%">
+        <img
+          src="../assets/projects/edlib.jpg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body" style="">
+          <h5 class="card-text text-black-50">
+            This is a wider card with supporting text below as a n atural
+            lead-in to additional content. his is a wider card with supporting
+            text below as a n his is a wider card with supporting text below as
+            a n This content is a little bit longer.
+          </h5>
+          <div>
+            <h6>Link :</h6>
+            <a href=""><h6>https://githup.com</h6> </a>
+          </div>
+        </div>
+      </div>
+      <div class="container" style="width: 50%">
+        <div class="row">
+          <div class="col-sm">
+            <div class="d-grid gap-2 d-md-block">
+              <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div class="col-sm">
+            <button class="btn btn-primary" type="button">Video Game</button>
+          </div>
+          <div class="col-sm">
+            <h3>GY-ball</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 container" style="width: 80%">
+        <img
+          src="../assets/projects/gyroball.jpg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-text text-black-50">
+            This is a wider card with supporting text below as a n atural
+            lead-in to additional content. his is a wider card with supporting
+            text below as a n his is a wider card with supporting text below as
+            a n This content is a little bit longer.
+          </h5>
+          <div>
+            <h6>Link :</h6>
+            <a href=""><h6>https://githup.com</h6> </a>
+          </div>
+        </div>
+      </div>
+      <div class="container" style="width: 50%">
+        <div class="row">
+          <div class="col-sm">
+            <div class="d-grid gap-2 d-md-block">
+              <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div class="col-sm">
+            <button class="btn btn-primary" type="button">Video Game</button>
+          </div>
+          <div class="col-sm">
+            <h3>GYRO-DELIB</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 container" style="width: 80%">
+        <img src="../assets/projects/urba.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-text text-black-50">
+            This is a wider card with supporting text below as a n atural
+            lead-in to additional content. his is a wider card with supporting
+            text below as a n his is a wider card with supporting text below as
+            a n This content is a little bit longer.
+          </h5>
+          <div>
+            <h6>Link :</h6>
+            <a href=""><h6>https://githup.com</h6> </a>
+          </div>
+        </div>
+      </div>
+      <div class="container" style="width: 50%">
+        <div class="row">
+          <div class="col-sm">
+            <div class="d-grid gap-2 d-md-block">
+              <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div class="col-sm">
+            <button class="btn btn-primary" type="button">Website</button>
+          </div>
+          <div class="col-sm">
+            <h3>Lahrence-BOUHRANCE</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 container" style="width: 80%">
+        <img
+          src="../assets/projects/boursoweb.jpg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-text text-black-50">
+            This is a wider card with supporting text below as a n atural
+            lead-in to additional content. his is a wider card with supporting
+            text below as a n his is a wider card with supporting text below as
+            a n This content is a little bit longer.
+          </h5>
+          <div>
+            <h6>Link :</h6>
+            <a href=""><h6>https://githup.com</h6> </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +212,8 @@ export default {
 </script>
 
 <style>
-
+hr{
+    border: 1px solid;
+    width: 40%;
+}
 </style>
